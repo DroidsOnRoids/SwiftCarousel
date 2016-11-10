58647ac6997cff3cd46dc60c0369d2be0d361995
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2016-11-11 00:13:35 +0100

    [Example2] Update to Swift 3.0

--- Examples/Example2/Example2/ViewController.swift
+++ Examples/Example2/Example2/ViewController.swift
@@ -48,11 +48,6 @@ class ViewController: UIViewController {
         
         view.addSubview(title)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
 
 extension ViewController: SwiftCarouselDelegate {
